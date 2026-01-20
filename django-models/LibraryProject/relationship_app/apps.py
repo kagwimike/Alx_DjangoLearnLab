cf7a0db5a40263b38e55a493451decf880ce8b0f
Mike Kagwi <mikekagwi440@gmail.com>
2026-01-20 10:10:07 +0300

    feat: add function-based and class-based views with URL config and templates

--- django-models/LibraryProject/relationship_app/apps.py
+++ django-models/LibraryProject/relationship_app/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
-
 class RelationshipAppConfig(AppConfig):
-    name = 'relationship_app'
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'LibraryProject.relationship_app'
