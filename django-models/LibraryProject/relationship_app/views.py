ebec57708fbc63c31bd579d815e2d4aedb4da375
Mike Kagwi <mikekagwi440@gmail.com>
2026-01-20 10:39:37 +0300

    fix: update LibraryDetailView template path to relationship_app/library_detail.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book, Library
 from django.views.generic import DetailView
-
+from .models import Library
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
