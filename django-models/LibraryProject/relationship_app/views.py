4a81cf1e6d694e9071534cdaf1d9ebf639739d91
Mike Kagwi <mikekagwi440@gmail.com>
2026-01-20 10:23:39 +0300

    add function-based and class-based views with URL config and templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Book, Library
 from django.views.generic import DetailView
+from .models import Book
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    # ALX expects the template path to include the app name
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to display details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library'
+
