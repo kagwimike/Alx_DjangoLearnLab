0b9fdd69be2210abd9e439b82ee9c936b3efcf75
Mike Kagwi <mikekagwi440@gmail.com>
2026-01-21 21:27:39 +0300

    Fix decorator imports for permissions and role-based views

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import permission_required, user_passes_test
 from django.views.generic.detail import DetailView
+
 from .models import Book, Library
-from django.contrib.auth.decorators import user_passes_test 
-import permission_required
+ 
 
 
 # -------- AUTH VIEW --------
