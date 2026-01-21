d9a5beec6e8e5de5a9792ba63674d8510166404e
Mike Kagwi <mikekagwi440@gmail.com>
2026-01-21 18:49:06 +0300

    User Aunthencation

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,15 +3,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(
-        template_name='relationship_app/login.html'
-    ), name='login'),
-
-    path('logout/', LogoutView.as_view(
-        template_name='relationship_app/logout.html'
-    ), name='logout'),
-
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-
-    path('books/', views.list_books, name='list_books'),
 ]
