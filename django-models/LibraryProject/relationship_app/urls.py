75dab94a02049747fc0de10133da94cd8075295f
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-07 23:19:52 +0300

    Update urls.py with add, edit, delete book paths

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-
-    # Authentication URLs
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', register, name='register'),
