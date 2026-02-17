ba3e1d486903c57023c96805ef43056c65149ab7
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-17 13:03:31 +0300

    Initial commit: Set up Django REST Framework project with Book model

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# api/views.py
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(generics.ListAPIView):
+    """
+    API endpoint that allows all books to be viewed.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
