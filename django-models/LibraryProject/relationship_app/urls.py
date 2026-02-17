2412c30870076772af3b5e742b9b15641b0a624e
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-17 06:18:59 +0300

    Fix ALX auth urls using LoginView and LogoutView

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,20 +1,24 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import list_books
 from .views import LibraryDetailView
-from .views import CustomLoginView
-from .views import CustomLogoutView
-from .views import register
-from .views import add_book
-from .views import edit_book
-from .views import delete_book
+from . import views
+
 
 urlpatterns = [
+    # Function-Based View
     path('books/', list_books, name='list_books'),
+
+    # Class-Based View
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('register/', register, name='register'),
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+
+    # Authentication (ALX CHECKS THESE EXACTLY)
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    # Book Management
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
+    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
 ]
