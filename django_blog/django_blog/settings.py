2b60d4a3bcbe097d9ff5df5bb3c63c22afe13675
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-18 06:19:03 +0300

    Configured MySQL database blogplatform

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -73,11 +73,14 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogplartform',
+        'USER': 'root',              # change if different
+        'PASSWORD': '', # put your MySQL password here
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
