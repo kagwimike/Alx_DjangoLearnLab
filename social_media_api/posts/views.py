42a7329afabc95625b13c493ebd8cfe1228b829d
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-22 13:21:48 +0300

    Task 2: Implement User Follow System and Feed Endpoint

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,10 +1,9 @@
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework import viewsets, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
@@ -18,10 +17,10 @@ class PostPagination(PageNumberPagination):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
     pagination_class = PostPagination
     filter_backends = [SearchFilter]
-    search_fields = ['title','content']
+    search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -30,7 +29,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -38,18 +37,13 @@ class CommentViewSet(viewsets.ModelViewSet):
 
 # ---------------- FEED VIEW ---------------- #
 @api_view(['GET'])
-@permission_classes([IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated])
 def user_feed(request):
     """
     Returns posts from users the current user is following,
     ordered by newest first.
     """
-    # Get all users the current user is following
     following_users = request.user.following.all()
-
-    # Query posts authored by followed users, newest first
     posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
-
-    # Serialize and return
     serializer = PostSerializer(posts, many=True)
     return Response(serializer.data)
