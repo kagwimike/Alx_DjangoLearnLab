97259b8426e01f0d6f61b13643ae717103884100
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-22 11:06:21 +0300

    Task 0: Setup Social Media API with Custom User Auth

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib.auth import authenticate
+from rest_framework.authtoken.models import Token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id','username','email','password','bio','profile_picture']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        Token.objects.create(user=user)
+        return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+
+        if user:
+            token, created = Token.objects.get_or_create(user=user)
+            return {'user': user, 'token': token.key}
+        raise serializers.ValidationError("Invalid credentials")
