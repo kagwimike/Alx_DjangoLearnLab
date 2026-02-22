72290605f596a7c7b046c43e46cef3ab63709589
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-22 13:19:04 +0300

    Task 2: Implement User Follow System and Feed Endpoint

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,14 +1,14 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
-from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
-from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+from .permissions import IsOwnerOrReadOnly
 
 
 class PostPagination(PageNumberPagination):
@@ -35,16 +35,21 @@ class CommentViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
+
+# ---------------- FEED VIEW ---------------- #
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 def user_feed(request):
-
+    """
+    Returns posts from users the current user is following,
+    ordered by newest first.
+    """
+    # Get all users the current user is following
     following_users = request.user.following.all()
 
-    posts = Post.objects.filter(
-        author__in=following_users
-    ).order_by('-created_at')
+    # Query posts authored by followed users, newest first
+    posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
+    # Serialize and return
     serializer = PostSerializer(posts, many=True)
-
-    return Response(serializer.data)       
+    return Response(serializer.data)
