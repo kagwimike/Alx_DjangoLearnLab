418b2bd2d529da01b3817fcfd575aba61a7c7232
Mike Kagwi <mikekagwi440@gmail.com>
2026-02-23 07:01:29 +0300

    Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,40 +1,27 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
-# FBV imports
-from .views import list_books
-from .views import add_book
-from .views import edit_book
-from .views import delete_book
-from .views import register
-
-# CBV imports
-from .views import LibraryDetailView
-
-# Role-based views
-from .views import admin_view
-from .views import librarian_view
-from .views import member_view
 
 urlpatterns = [
     # Function-Based View
-    path('list_books/', list_books, name='list_books'),
+    path('list_books/', views.list_books, name='list_books'),
 
-    # Book Management (ALX expects these exact paths)
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
-    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+    # Book Management
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
 
     # Class-Based View
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 
     # Authentication
-    path('register/', register, name='register'),
+    path('register/', views.register, name='register'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
     # Role-based views
-    path('admin-role/', admin_view, name='admin_view'),
-    path('librarian-role/', librarian_view, name='librarian_view'),
-    path('member-role/', member_view, name='member_view'),
+    path('admin-role/', views.admin_view, name='admin_view'),
+    path('librarian-role/', views.librarian_view, name='librarian_view'),
+    path('member-role/', views.member_view, name='member_view'),
 ]
